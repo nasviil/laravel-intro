d5547bf870eb3e6a60c49519f028bae5c965cff4
Kent Nasvil Madrid <itsfrnasviil@gmail.com>
2024-07-09 16:41:10 +0800

    changed file directory

---
+++ resources/views/etanom/pages/profile.blade.php
@@ -0,0 +1,27 @@
+@extends('etanom.layouts.dashboard')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <title>{{ $app_name }}</title>
+
+  <!-- Fonts -->
+  <link rel="stylesheet" href="{{ asset('fontawesome-free-6.5.2-web\css\all.min.css') }}">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap" rel="stylesheet" />
+
+  <!-- Scripts -->
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+  @section('content')
+  <section class="h-full flex-grow flex flex-col py-4 pr-14 pl-72 mr-[24%]">
+  </section>
+  @endsection
+
+</html>
